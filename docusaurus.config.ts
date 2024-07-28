346d1c64bdf3bdee91a3ca13018e094888c4b1e8
fenglyu.lin <shenqiaaa@gmail.com>
2024-07-28 18:59:18 +0800

    update Feiren Tech Summary title v2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,7 +84,7 @@ const config: Config = {
           type: 'doc',
           docId: 'practise-coding-record',
           position: 'right',
-          label: "纳米飞刃技术集/Feiren Tech Summary",
+          label: "纳米飞刃技术集",
         },
         {
           type: 'doc',
