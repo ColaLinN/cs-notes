d96df92071606733319d03824c9790186916b068
fenglyu.lin <shenqiaaa@gmail.com>
2025-02-28 01:57:00 +0800

    without context

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -76,32 +76,35 @@ const config: Config = {
       appId: 'JYA67C7803',
 
       // Public API key: it is safe to commit it
-      apiKey: 'e253041559bea558b7309ab840d22229',
+      apiKey: '880e03eeac519808fd841d5d26f4fca7',
 
       indexName: 'fenglyulin',
 
       // Optional: see doc section below
-      contextualSearch: true,
+      contextualSearch: false,
 
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: 'external\\.com|domain\\.com',
+      // externalUrlRegex: 'external\\.com|domain\\.com',
 
       // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      replaceSearchResultPathname: {
-        from: '/docs/', // or as RegExp: /\/docs\//
-        to: '/',
-      },
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
 
       // Optional: Algolia search parameters
-      searchParameters: {},
+      searchParameters: {
+        // initialQuery: 'test', // Optional
+      },
 
       // Optional: path for search page that enabled by default (`false` to disable it)
       searchPagePath: 'search',
 
       // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-      insights: false,
+      // insights: false,
 
       //... other Algolia params
+      
     },
 
     navbar: {
